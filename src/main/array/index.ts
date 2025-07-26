3e6331be62f049a3d6b42ca8001997b295e6631f
Henry Box <14365005+henrybox@users.noreply.github.com>
2025-07-26 17:22:31 -0300

    WIP: typescript migration

---
+++ src/main/array/index.ts
@@ -0,0 +1,36 @@
+import Property from '../property';
+import Items from './items';
+
+export default class extends Property {
+	get dp() {
+		return 'utils.config.property.array';
+	}
+
+	get is() {
+		return 'array';
+	}
+
+	get errors() {
+		return super.errors.concat(this.#items.errors);
+	}
+
+	#items;
+	get items() {
+		return this.#items;
+	}
+
+	constructor(path, branchesSpecs, branch, parent) {
+		super(path, branchesSpecs, branch, parent);
+		this.#items = new Items(this);
+	}
+
+	_process() {
+		if (super._process() === false) return false;
+		return this.#items.update();
+	}
+
+	destroy() {
+		super.destroy();
+		this.#items.destroy();
+	}
+}
---
+++ src/main/index.ts
@@ -0,0 +1,5 @@
+import Object from './object';
+import Collection from './elements/collection';
+
+export /*bundle*/ const Config = Object;
+export /*bundle*/ const ConfigCollection = Collection;
